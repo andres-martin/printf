410e7948b6a6dd239002af86b29eb4fd43f72132
andres-martin <andresmartin266@gmail.com>
2019-03-16 21:56:32 +0000

    [feature] fucntion printf test created

---
+++ test/_printftest1.c
@@ -0,0 +1,84 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * p_int - prints integer
+ * @args: va_list
+*/
+void p_int(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+/**
+ * p_char - prints char
+ * @args: va_list
+*/
+void p_char(va_list args)
+{
+	char ch;
+	
+	ch = va_arg(args, int);
+	_putchar(ch);
+}
+/**
+ * p_float - prints float
+ * @args: va_list
+*/
+void p_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * p_string - prints string
+ * @args: va_list
+*/
+void p_string(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char*);
+	if (s)
+		printf("%s", s);
+	else
+		printf("%p", s);
+}
+/**
+ * print_all - prints anything
+ * @format: const pointer to a char
+*/
+int _printf(const char * format, ...)
+{
+	int i, j;
+
+	fn_t fmt[] = {
+		{"%c", p_char},
+		{"%i", p_int},
+		{"%f", p_float},
+		{"%s", p_string},
+		{NULL, NULL}
+	};
+
+	va_list args;
+
+	va_start(args, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (fmt[j].ob != NULL)
+		{
+			if (format[i] == *fmt[j].ob)
+			{
+				fmt[j].type(args);
+				break;
+			}
+			j++;
+		}
+	/*if (fmt[j].ob != NULL && format[i + 1] != '\0')
+	printf(", ");*/
+	i++;
+	}
+va_end(args);
+_putchar(10);
+return (0);
+}
---
+++ test/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdarg.h>
+/**
+ * struct fn - Struct fn
+ * @ob: The operator
+ * @type: The function associated
+*/
+typedef struct fn
+{
+	char *ob;
+	void (*type)(va_list var);
+} fn_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
