d909a4d38d4ba8d1b917588c4ae7f2918ebc1c9f
andres-martin <andresmartin266@gmail.com>
2019-03-18 23:22:42 +0000

    [feature] new functions added to the _printf

--- printf_v3/0-main.c
+++ printf_v3/0-main.c
@@ -14,12 +14,17 @@ int main(void)
   unsigned int ui;
   void *addr;
 
-  len = _printf("Let's try to printf a simple sentence.%c\n", "\n");
-  len2 = printf("Let's try to printf the original a simple sentence.%c\n", "\n");
+  _printf("Character:[%c]\n", 'H');
+  printf("Character:[%c]\n", 'H');
+  len = _printf("Complete the sentence: You %s nothig. Jon snow. \n", (char *)0);
+  len2 = printf("Complete the sentence: You %s nothig. Jon snow. \n", (char *)0);
+  printf("String:[%s]\n", "I am a string !");
+   _printf(NULL);
+  printf(NULL);
   ui = (unsigned int)INT_MAX + 1024;
   addr = (void *)0x7ffe637541f0;
-  _printf("Length:[%d, %i]\n", len, len);
-  printf("Length:[%d, %i]\n", len2, len2);
+  _printf("Length yo:[%d, %i]\n", len, len);
+  printf("Length ellos:[%d, %i]\n", len2, len2);
   _printf("Negative:[%d]\n", -762534);
   printf("Negative:[%d]\n", -762534);
   _printf("Unsigned:[%u]\n", ui);
@@ -38,7 +43,8 @@ int main(void)
   len2 = printf("Percent:[%%]\n");
   _printf("Len:[%d]\n", len);
   printf("Len:[%d]\n", len2);
-  _printf("Unknown:[%r]\n");
+	_printf("%b\n", 98);
+  _printf("Unknown:[%r]\n", "reversar");
   printf("Unknown:[%r]\n");
   return (0);
 }
