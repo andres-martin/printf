992f197d0591c75366cfb8ea0a188838c3f23db9
Heimer <732@holberton.com>
2019-03-18 15:19:43 +0000

    [github] update main.c

--- test2/0-main.c
+++ test2/0-main.c
@@ -11,6 +11,8 @@ int main(void)
 {
   	int len;
   	int len2;
+//	_printf("%%%%%%%r%%%%%");
+//	printf("%%%%%%%r%%%%%");
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("Length:[%d, %i]\n", len, len);
